5cf66a2b88736f7b9ce3a4b395a89899dd982b0f
Juan Castro <info@jempe.org>
2021-05-17 09:42:56 -0500

    MacOS setup fix

--- setup_scripts/setup_go.sh
+++ setup_scripts/setup_go.sh
@@ -16,15 +16,19 @@ else
 	mkdir -p $GO_PATH
 fi
 
-if grep -q "GOROOT=" "$BASHRC_FILE";
+if grep -q "GOPATH=" "$BASHRC_FILE";
 then
-	echo "GOROOT variable already setup"
+	echo "GOPATH variable already setup"
 else
 	echo "setting up go enviroment variables"
 	echo " " >> $BASHRC_FILE
 	echo "#GOLANG environment variables" >> $BASHRC_FILE
-	echo 'export GOROOT=/opt/go' >> $BASHRC_FILE
-	echo 'export PATH=$GOROOT/bin:$PATH' >> $BASHRC_FILE
+
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		echo 'export GOROOT=/opt/go' >> $BASHRC_FILE
+		echo 'export PATH=$GOROOT/bin:$PATH' >> $BASHRC_FILE
+	fi
+
 	echo "export GOPATH=$GO_PATH" >> $BASHRC_FILE
 	echo 'export GOBIN=$GOPATH/bin' >> $BASHRC_FILE
 	echo 'export PATH=$PATH:$GOBIN' >> $BASHRC_FILE
