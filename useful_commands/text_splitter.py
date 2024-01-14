00d3395ec345c1faa78223bf3ccd5d91751ae744
Jempe <info@jempe.org>
2024-01-14 11:22:36 -0500

    divide in chunks that don't exceed the token limit

--- useful_commands/text_splitter.py
+++ useful_commands/text_splitter.py
@@ -5,6 +5,31 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import sys
 import tiktoken
+import argparse
+
+#arg chunk_size gets the number of tokens per chunk
+#arg chunk_overlap gets the number of tokens to overlap between chunks
+#arg total_tokens gets the total number of tokens to display
+
+parser = argparse.ArgumentParser(description="Split the text in multiple chunks to use in chatGPT")
+
+# Add command-line arguments
+parser.add_argument('-total_tokens', type=int, help='Total number of tokens')
+parser.add_argument('-filename', type=str, help='Filename to split')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+
+total_tokens = 2500
+
+if args.total_tokens:
+    total_tokens = args.total_tokens
+
+
+chunk_size = total_tokens / 5
+chunk_overlap = 0
+
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """Returns the number of tokens in a text string."""
@@ -13,11 +38,7 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     return num_tokens
 
 
-if len(sys.argv) < 2:
-    print("Usage: python text_splitter.py <filename>")
-    sys.exit(1)
-
-filename = sys.argv[1]
+filename = args.filename
 
 with open(filename, 'r') as f:
     filetext = f.read()
@@ -27,14 +48,37 @@ with open(filename, 'r') as f:
 #docs = splitter.create_documents([filetext])
 
 splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-    chunk_size=3000, chunk_overlap=50, encoding_name="cl100k_base"
+    chunk_size=chunk_size, chunk_overlap=chunk_overlap, encoding_name="cl100k_base"
 )
 
 texts = splitter.split_text(filetext)
 
-for text in texts:
+texts_data = []
+
+tokens_count = 0
+
+for i, text in enumerate(texts):
     tokens = num_tokens_from_string(text, "cl100k_base")
 
-    text = text.replace('\n', ' ') + '\n[CL100KTOKENS:' + str(tokens) + ']\n\n'
+    tokens_count += tokens
+
+    if tokens_count > total_tokens:
+        texts_data[i - 1]["split"] = True
+        tokens_count = tokens
+
+    texts_data.append({"text": text, "tokens": tokens, "split": False})
+
+tokens_count = 0
+
+for i, text_data in enumerate(texts_data):
+
+    tokens_count += text_data["tokens"]
+
+    text = text_data["text"]
+
     print(text)
+
+    if text_data["split"] or i == len(texts_data) - 1:
+        print('\n[CL100KTOKENS:' + str(tokens_count) + ']\n\n')
+        tokens_count = 0
 
